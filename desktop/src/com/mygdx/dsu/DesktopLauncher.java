106a80e8325f1384178f3bf6468b2ea66c1441a5
Benoit Bertrand <benoit.bertrand@live.fr>
2022-06-27 01:04:00 +0200

    First Commit

---
+++ desktop/src/com/mygdx/dsu/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.dsu;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.dsu.DSUGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Dungeons Stories Universe");
+		new Lwjgl3Application(new DSUGame(), config);
+	}
+}
